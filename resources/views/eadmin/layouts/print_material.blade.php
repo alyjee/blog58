b426e5b8f825174ca3ee65eca67e6b3e0e3de48d
Tahir Raza <tahir.raza@ilsainteractive.com>
2019-07-04 17:08:05 +0500

    modified print layout with minimal css

--- resources/views/eadmin/layouts/print_material.blade.php
+++ resources/views/eadmin/layouts/print_material.blade.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html>
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,71 +9,14 @@
     <link rel="icon" type="image/png" sizes="16x16" href="../plugins/images/favicon.png">
     <title>Sea Links - Lowest Fare</title>
 
-    
-    <!-- Bootstrap Core CSS -->
-    <link href="{{ asset('public/eadmin/material/bootstrap/dist/css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('public/eadmin/plugins/bower_components/bootstrap-extension/css/bootstrap-extension.css') }}" rel="stylesheet">
+    <link rel="stylesheet" media="all" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
 
 </head>
-
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" type="image/png" sizes="16x16" href="../plugins/images/favicon.png">
-    <title>SeaLinks - Admin</title>
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    
-    <!-- Bootstrap Core CSS -->
-    <link href="{{ asset('public/eadmin/material/bootstrap/dist/css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('public/eadmin/plugins/bower_components/bootstrap-extension/css/bootstrap-extension.css') }}" rel="stylesheet">
-    
-    <!-- Editable CSS -->
-    <link type="text/css" rel="stylesheet" href="{{ asset('public/eadmin/plugins/bower_components/jsgrid/dist/jsgrid.min.css') }}" />
-    <link type="text/css" rel="stylesheet" href="{{ asset('public/eadmin/plugins/bower_components/jsgrid/dist/jsgrid-theme.min.css') }}" />
-
-
-    <!-- Menu CSS -->
-    <link href="{{ asset('public/eadmin/plugins/bower_components/sidebar-nav/dist/sidebar-nav.min.css') }}" rel="stylesheet">
-
-    <!--alerts CSS -->
-    <link href="{{ asset('public/eadmin/plugins/bower_components/sweetalert/sweetalert.css')}}" rel="stylesheet" type="text/css">
-
-    <!-- toast CSS -->
-    <link href="{{ asset('public/eadmin/plugins/bower_components/toast-master/css/jquery.toast.css') }}" rel="stylesheet">
-
-    <!-- Date picker plugins css -->
-    <link href="{{ asset('public/eadmin/plugins/bower_components/bootstrap-datepicker/bootstrap-datepicker.min.css') }}" rel="stylesheet" type="text/css" />
-
-    <!-- morris CSS -->
-    <link href="{{ asset('public/eadmin/plugins/bower_components/morrisjs/morris.css') }}" rel="stylesheet">
-    <!-- animation CSS -->
-    <link href="{{ asset('public/eadmin/material/css/animate.css')}}" rel="stylesheet">
-    <!-- Custom CSS -->
-    <link href="{{ asset('public/eadmin/material/css/style.css')}}" rel="stylesheet">
-    <link href="{{ asset('public/css/custom.css')}}" rel="stylesheet">
-    <!-- color CSS -->
-    <link href="{{ asset('public/eadmin/material/css/colors/default.css')}}" id="theme" rel="stylesheet">
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-<![endif]-->
-    <script type="text/javascript" >
-        siteUrl = '{{ URL::to('/') }}';
-    </script>
-</head>
-
 <body>
-    <div id="wrapper">
+    <div id="wrapper" >
         <div class="row">
-            <div class="col-sm-12 text-center" style="padding: 20px;">
-                <img src="{{ asset('public/images/sealinks-logo.jpg') }}" alt="home" style="max-width: 100px;">
+            <div class="col-xs-12 text-center" style="padding: 20px;">
+                <img src="{{ public_path('images/sealinks-logo.jpg') }}" alt="home" style="max-width: 100px;">
             </div>
         </div>
         <div class="container-fluid">
@@ -85,7 +27,4 @@
     </div>
     <!-- /#wrapper -->
 </body>
-<script type="text/javascript">
-    window.print()
-</script>
 </html>
