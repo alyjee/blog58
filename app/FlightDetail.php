95b1ac2c312d8b1af984876d20f6ccda047b9a56
Tahir Raza <tahir.raza@ilsainteractive.com>
2019-07-09 17:35:56 +0500

    added new model for flight details

---
+++ app/FlightDetail.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use DB;
+
+class FlightDetail extends Model
+{
+    //
+    public $timestamps = true;
+
+    protected $fillable = ['id', 'form_id', 'day', 'date', 'flight_status', 'city_terminal_stopover', 'time', 'flight_class_status', 'stop_eqp_flts', 'archive', 'created_at', 'updated_at'];
+
+    /*
+    *
+    * Database Relations
+    */
+
+    
+    
+}
